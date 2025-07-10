8246c5103f271d931421757fab34ce99f6614e84
Adam J. Lincoln <adamjlincoln@gmail.com>
2025-07-09 23:00:56 -0400

    get takes name or model

--- pydiditbackend/__init__.py
+++ pydiditbackend/__init__.py
@@ -1,6 +1,8 @@
 """The primary API for pydiditbackend."""
 
 import os
+from collections.abc import Iterable
+from typing import overload
 
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker as sqlalchemy_sessionmaker
@@ -18,10 +20,22 @@ def prepare(provided_sessionmaker: sqlalchemy_sessionmaker) -> None:
     globals()["sessionmaker"] = provided_sessionmaker
     models.prepare(provided_sessionmaker)
 
-def get(model: str) -> models.Base:
+@overload
+def get(model: str) -> Iterable[models.Base]:
+    ...
+
+@overload
+def get(model: models.Base) -> Iterable[models.Base]:
+    ...
+
+def get(model):
     """Get instances."""
+    model = getattr(models, model) if isinstance(model, str) else model
+    query = select(model)
+    if hasattr(model, "display_position"):
+        query = query.order_by(model.display_position)
     with sessionmaker() as session:  # noqa: F821
-        return session.scalars(select(getattr(models, model))).one()
+        return session.scalars(query).all()
 
 def put(model: models.Base) -> models.Base:
     """Put an instance."""
@@ -31,8 +45,8 @@ def put(model: models.Base) -> models.Base:
 
 if __name__ == "__main__":
     prepare(sqlalchemy_sessionmaker(create_engine(os.environ["DB_URL"])))
-    #print(get("Todo"))
-    put(models.Todo(  # type: ignore[attr-defined]
-        description="fake",
-        state=models.enums.State.active,
-    ))
+    print(get("Todo"))
+    #put(models.Todo(  # type: ignore[attr-defined]
+        #description="fake",
+        #state=models.enums.State.active,
+    #))
